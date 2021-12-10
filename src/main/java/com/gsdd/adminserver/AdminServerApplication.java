b64729c98f0aa72bf4df46f884fa8c92c55bd8c4
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-10 11:05:08 -0500

    Update new code group

--- src/main/java/com/gsdd/adminserver/AdminServerApplication.java
+++ src/main/java/com/gsdd/adminserver/AdminServerApplication.java
@@ -1,16 +1,15 @@
-package co.com.gsdd.adminserver;
+package com.gsdd.adminserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
 public class AdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AdminServerApplication.class, args);
+  }
 
 }
--- src/test/java/com/gsdd/adminserver/AdminServerApplicationTests.java
+++ src/test/java/com/gsdd/adminserver/AdminServerApplicationTests.java
@@ -1,4 +1,4 @@
-package co.com.gsdd.adminserver;
+package com.gsdd.adminserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AdminServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
