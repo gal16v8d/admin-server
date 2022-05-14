87a25be0dfb38f0c51435a77a37e71466b436a4c
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-05-14 00:11:43 -0500

    Switch from AOSP style

--- src/main/java/com/gsdd/adminserver/AdminServerApplication.java
+++ src/main/java/com/gsdd/adminserver/AdminServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAdminServer
 public class AdminServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdminServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AdminServerApplication.class, args);
+  }
 }
