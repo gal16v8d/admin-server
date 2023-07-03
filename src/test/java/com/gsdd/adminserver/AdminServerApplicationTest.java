a0e4fc028dfb4ab874672e5c73ddc55028656423
alexg <alex.galvis.sistemas@gmail.com>
2023-07-03 15:52:41 -0500

    Update project dependencies

--- src/test/java/com/gsdd/adminserver/AdminServerApplicationTest.java
+++ src/test/java/com/gsdd/adminserver/AdminServerApplicationTest.java
@@ -1,11 +1,15 @@
 package com.gsdd.adminserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class AdminServerApplicationTest {
 
   @Test
-  void contextLoads() {}
+  void contextLoads(ApplicationContext context) {
+    Assertions.assertNotNull(context);
+  }
 }
